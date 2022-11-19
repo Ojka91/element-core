0010c6ee5a91dcebe064f9e93b9cde3f11a197d6
Marc Lopez <marclopezgarcia@gmail.com>
2022-11-19 17:14:06 +0100

    Added a folder named schemas where messages between client-server are described.

---
+++ src/schemas/player_actions.ts
@@ -0,0 +1,65 @@
+enum Events {
+    Piece = "Piece",
+    EndTurn = "EndTurn"
+}
+
+enum Pieces {
+    Sage = "Sage",
+    Water = "Water",
+    Fire = "Fire",
+    Earth = "Earth",
+    Wind = "Wind"
+}
+
+enum Elements {
+    Sage = "Sage",
+    Water = "Water",
+    Fire = "Fire",
+    Earth = "Earth",
+    Wind = "Wind"
+}
+
+type Position = {
+    row: number;
+    column: number;
+}
+
+type WaterReaction = {
+    initial_river: Array<Position>;
+    new_river: Array<Position>;
+}
+
+type Reactions = {
+    water: WaterReaction;
+    fire: null;
+    earth: null;
+    wind: null;
+}
+
+type Piece = {
+    position: Position;
+    type: Pieces;
+}
+
+type MessageHeader = {
+    room_uuid: string;
+    player_uuid: string;
+}
+
+type PlacePieceBody = {
+    piece: Piece;
+    reaction: Reactions | null
+}
+
+export type PlacePieceSchema = MessageHeader & {
+    event_type: Events;
+    body: PlacePieceBody | null;
+}
+
+export type StartOfTurnSchema = MessageHeader & {
+    requested_pieces: Array<Elements>;
+}
+
+export type EndOfTurnSchema = MessageHeader & {
+    
+}
