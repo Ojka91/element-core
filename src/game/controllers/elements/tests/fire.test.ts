b76c8a75624607f5eb56b96558f3910cd2afcdc4
Oscar Lucena Papiol <oscar@Oscars-MacBook-Pro.local>
2022-12-16 09:42:21 +0100

    Fixed fire reaction

--- src/game/controllers/elements/tests/fire.test.ts
+++ src/game/controllers/elements/tests/fire.test.ts
@@ -48,6 +48,7 @@ describe('FireModelController: reaction', () => {
    it('Reaction: Placing fire without any fire close, should not trigger reaction', async () => {
       /* Not propagation
           PF: Place fire
+          W: Wind piece
            0   1   2   3   4   5   6   7   8   9 
          -----------------------------------------
       0  |   |   |   |   |   |   |   |   |   |   |
@@ -56,7 +57,7 @@ describe('FireModelController: reaction', () => {
          -----------------------------------------
       2  |   |   |   |   |   |   |   |   |   |   |
          -----------------------------------------
-      3  |   |   |   |   | PF|   |   |   |   |   |
+      3  |   |   |   |   | PF| W  |   |   |   |   |
          -----------------------------------------
       4  |   |   |   |   |   |   |   |   |   |   |
          -----------------------------------------
@@ -74,6 +75,10 @@ describe('FireModelController: reaction', () => {
       const pf_pos: Position = {
          row: 3,
          column: 4
+      }
+      const wind_pos: Position = {
+         row: 3,
+         column: 5
       }
       const left: Position = {
          row: 3,
@@ -93,15 +98,19 @@ describe('FireModelController: reaction', () => {
       }
 
       const fire: FireModel = new FireModel();
+      const wind: WindModel = new WindModel();
 
+      new WindController(wind).place(grid, wind_pos);
+      new FireController(fire).place(grid, pf_pos);
       new FireController(fire).place(grid, pf_pos);
 
       new FireController(fire).reaction(grid, pf_pos, element_pool_manager);
 
       expect(grid_controller.isPositionEmpty(bottom)).toBe(true);
       expect(grid_controller.isPositionEmpty(top)).toBe(true);
-      expect(grid_controller.isPositionEmpty(right)).toBe(true);
+      expect(grid_controller.isPositionEmpty(right)).toBe(false);
       expect(grid_controller.isPositionEmpty(left)).toBe(true);
+      expect(grid_controller.isWindCell(wind_pos)).toBe(true);
 
    })
    it('Reaction: Should propagate free fire orthogonally left', async () => {
@@ -614,11 +623,14 @@ describe('FireModelController: reaction', () => {
 
 
 
+      expect(grid_controller.isFireCell(w_pos)).toBe(false);
+      expect(grid_controller.isWindCell(w_pos)).toBe(true);
 
 
       new FireController(placed_fire).reaction(grid, pf_pos, element_pool_manager);
 
       expect(grid_controller.isFireCell(ff_pos)).toBe(true);
+      expect(grid_controller.isFireCell(w_pos)).toBe(true);
 
    })
 
