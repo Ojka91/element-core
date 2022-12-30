a734436c7e64e9d21950a8a7aaa9d7298108a4cb
Oscar Lucena Papiol <oscar@ip-192-168-18-143.eu-west-1.compute.internal>
2022-12-30 12:12:40 +0100

    Small bugfix

--- src/game/controllers/elements/tests/water.test.ts
+++ src/game/controllers/elements/tests/water.test.ts
@@ -843,61 +843,59 @@ describe('WaterController: reaction', () => {
                 vertical river
       
          */
+        
+            const placed_water_pos: Position = { row: 3, column: 3 }
+        
+            const river_1: Array<Position> = [
+                { row: 3, column: 4 },
+                { row: 3, column: 5 }
+            ]
+            
+            const river_2: Array<Position> = [
+                { row: 4, column: 3 },
+                { row: 5, column: 3 },
+                { row: 6, column: 3 }
+            ]
+        
+            const rivers: Array<Array<Position>> = [river_1, river_2];
+        
+            const waterPositions: Array<Position> = [
+                { row: 2, column: 4 },
+                { row: 3, column: 4 },
+                { row: 3, column: 5 },
+                { row: 4, column: 3 },
+                { row: 5, column: 3 },
+                { row: 6, column: 3 },
+                { row: 6, column: 4 }
+            ]
+        
+            const firePosition: Position = {
+                row: 3,
+                column: 6
+            }
+        
+            const grid: GridModel = new GridModel();
+            const grid_controller: GridController = new GridController(grid);
+            grid_controller.generateInitialGrid(10, 8);
+        
+            const element_pool_manager_model: ElementPoolManagerModel = new ElementPoolManagerModel()
+            const element_pool_manager: ElementPoolManager = new ElementPoolManager(element_pool_manager_model)
+        
+            // Prepare grid
+            waterPositions.forEach((water_pos) => {
+                const water: WaterModel = new WaterModel();
+                new WaterController(water).place(grid, water_pos, element_pool_manager);
+        
+            })
+        
+            const fire: FireModel = new FireModel();
+            new FireController(fire).place(grid, firePosition, element_pool_manager);
+        
+            // Place water
+            const placed_water: WaterModel = new WaterModel();
+            new WaterController(placed_water).place(grid, placed_water_pos, element_pool_manager);
+        
+            expect(new WaterController(placed_water).getRivers(grid, placed_water_pos).sort()).toEqual(rivers.sort());
     })
-
-    const placed_water_pos: Position = { row: 3, column: 3 }
-
-    const river_1: Array<Position> = [
-        { row: 3, column: 3 },
-        { row: 3, column: 4 },
-        { row: 3, column: 5 }
-    ]
-    
-    const river_2: Array<Position> = [
-        { row: 3, column: 3 },
-        { row: 4, column: 3 },
-        { row: 5, column: 3 },
-        { row: 6, column: 3 }
-    ]
-
-    const rivers: Array<Array<Position>> = [river_1, river_2];
-
-    const waterPositions: Array<Position> = [
-        { row: 2, column: 4 },
-        { row: 3, column: 4 },
-        { row: 3, column: 5 },
-        { row: 4, column: 3 },
-        { row: 5, column: 3 },
-        { row: 6, column: 3 },
-        { row: 6, column: 4 }
-    ]
-
-    const firePosition: Position = {
-        row: 3,
-        column: 6
-    }
-
-    const grid: GridModel = new GridModel();
-    const grid_controller: GridController = new GridController(grid);
-    grid_controller.generateInitialGrid(10, 8);
-
-    const element_pool_manager_model: ElementPoolManagerModel = new ElementPoolManagerModel()
-    const element_pool_manager: ElementPoolManager = new ElementPoolManager(element_pool_manager_model)
-
-    // Prepare grid
-    waterPositions.forEach((water_pos) => {
-        const water: WaterModel = new WaterModel();
-        new WaterController(water).place(grid, water_pos, element_pool_manager);
-
-    })
-
-    const fire: FireModel = new FireModel();
-    new FireController(fire).place(grid, firePosition, element_pool_manager);
-
-    // Place water
-    const placed_water: WaterModel = new WaterModel();
-    new WaterController(placed_water).place(grid, placed_water_pos, element_pool_manager);
-
-    expect(new WaterController(placed_water).getRivers(grid, placed_water_pos).sort()).toEqual(rivers.sort());
 
 })
